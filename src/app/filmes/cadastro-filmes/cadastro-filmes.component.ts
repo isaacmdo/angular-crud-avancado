20639768d9e3c97ea7da6c3ffafc23d3edc41071
Isaac <isaacestudo1999@gmail.com>
2021-10-31 21:43:07 -0300

    ...

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,24 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
 
 @Component({
   selector: 'dio-cadastro-filmes',
   templateUrl: './cadastro-filmes.component.html',
-  styleUrls: ['./cadastro-filmes.component.scss']
+  styleUrls: ['./cadastro-filmes.component.scss'],
 })
 export class CadastroFilmesComponent implements OnInit {
-
   options: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+    public validacao: ValidarCamposService) {}
+
+  get f() {
+    return this.options.controls;
+  }
 
   ngOnInit() {
-
     this.options = this.fb.group({
+      titulo: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(256),
+        ],
+      ],
+      urlFoto: ['', [Validators.required, Validators.minLength(10)]],
+      dataLancamento: ['', [Validators.required]],
+      descricao: [''],
+      nota: [
+        0,
+        [
+          Validators.required,
+          Validators.minLength(0),
+          Validators.maxLength(10),
+        ],
+      ],
+      urlImdb: ['', [Validators.minLength(10)]],
+      genero: ['', [Validators.required]],
       hideRequired: false,
       floatLabel: 'auto',
     });
-
   }
 
+  salvar(): void {
+    this.options.markAllAsTouched();
+    if (this.options.invalid) return;
+    console.log('entrou');
+    alert('SUCESSO!!!\n\n' + JSON.stringify(this.options.value, null, 4));
+  }
+
+  reiniciarForm(): void {
+    this.options.reset;
+  }
 }
---
+++ src/app/shared/components/campos/validar-campos.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarCamposService {
+
+  constructor() { }
+
+  hasError(control: AbstractControl, errorName: string): boolean {
+    return control.hasError(errorName);
+  }
+
+  hasErrorValidated(control: AbstractControl, errorName: string): boolean {
+    if((control.dirty || control.touched) && this.hasError(control, errorName)) {
+      return true; 
+    } else {
+      return false;
+    }
+  }
+}
