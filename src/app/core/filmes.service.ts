9b1a9cc00ffd94ef97f47661a31a8d158d1e476a
Isaac <isaacestudo1999@gmail.com>
2021-11-09 22:07:17 -0300

    consulta async / debounceTime

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ConfigParams } from '../shared/models/config-params';
 import { Filme } from '../shared/models/filme';
+import { ConfigParamsService } from './config-params.service';
 
 const url = 'http://localhost:3000/filmes/';
 
@@ -9,16 +11,17 @@ const url = 'http://localhost:3000/filmes/';
   providedIn: 'root',
 })
 export class FilmesService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigParamsService,
+  ) {}
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme);
   }
 
-  listar(pagina: number, qtdPagina: number): Observable<Filme[]> {
-    let httpParams = new HttpParams();
-    httpParams = httpParams.set('_page', pagina.toString());
-    httpParams = httpParams.set('_limit', qtdPagina.toString());
-    return this.http.get<Filme[]>(url, { params: httpParams });
+  listar(config: ConfigParams): Observable<Filme[]> {
+    const configParams = this.configService.configurarParametros(config);
+    return this.http.get<Filme[]>(url, { params: configParams });
   }
 }
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,20 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { FilmesService } from 'src/app/core/filmes.service';
+import { ConfigParams } from 'src/app/shared/models/config-params';
 import { Filme } from 'src/app/shared/models/filme';
-
+import { debounceTime } from 'rxjs/operators';
 @Component({
   selector: 'dio-listagem-filmes',
   templateUrl: './listagem-filmes.component.html',
   styleUrls: ['./listagem-filmes.component.scss'],
 })
 export class ListagemFilmesComponent implements OnInit {
-  readonly qtdPagina = 4;
-  pagina = 0;
-  filmes: Filme[] = []
+  readonly imagemPadrao = 'http://www.ccta.ufpb.br/labeet/contents/acervos/categorias/cordofones/udecra/sem-imagem.jpg/@@images/image.jpeg'
 
-  constructor(private filmesService: FilmesService) {}
+  config: ConfigParams = {
+    pagina: 0,
+    limite:  4
+  }
+  filmes: Filme[] = []
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
+
+  constructor(private filmesService: FilmesService,
+    private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.filtrosListagem.get('texto')
+    .valueChanges
+    .pipe(debounceTime(400))
+    .subscribe((val: string) => {
+      this.config.pesquisa = val;
+      this.resetarConsulta();
+    })
+
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
+      this.config.campo = {tipo: 'genero', valor: val};
+      this.resetarConsulta();
+    })
+
+
+    this.generos = [
+      'Acao',
+      'Romance',
+      'Aventura',
+      'Terror',
+      'Ficcao cientifica',
+      'Comedia',
+      'Aventura',
+      'Drama',
+    ];
+
     this.listarFilmes();
   }
 
@@ -23,9 +62,15 @@ export class ListagemFilmesComponent implements OnInit {
   }
 
   private listarFilmes(): void {
-    this.pagina++;
+    this.config.pagina++;
     this.filmesService
-      .listar(this.pagina, this.qtdPagina)
+      .listar(this.config)
       .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void {
+    this.config.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
   }
 }
